93ec83f94e8314619ae5890d7913014c29be996c
LinuxSoul <linuxsoul16@gmail.com>
2025-05-29 22:11:08 +1000

    TIB-14 "Added demo unit"

--- src/quickfind.tsx
+++ src/quickfind.tsx
@@ -1,16 +1,38 @@
-import { ActionPanel, Detail, List, Action, Icon } from "@raycast/api";
+import { List, Icon } from "@raycast/api";
 
 export default function Command() {
   return (
     <List>
       <List.Item
         icon={Icon.Bird}
+        title="Brisbane"
+        subtitle="GMT+10"
+        accessories={[{ text: `10:00 pm`, icon: Icon.Clock }]}
+        
+      />
+      <List.Item
+        icon={Icon.Bird}
+        title="bob"
+      />
+      <List.Item
+        icon={Icon.Bird}
         title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
+      />
+      <List.Item
+        icon={Icon.Bird}
+        title="sfsfs"
+      />
+      <List.Item
+        icon={Icon.Bird}
+        title="dad"
+      />
+      <List.Item
+        icon={Icon.Bird}
+        title="baba"
+      />
+      <List.Item
+        icon={Icon.Bird}
+        title="hghg"
       />
     </List>
   );
